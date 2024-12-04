fcceb040fc5c845493ae65ddbcff6df0197c6302
KaiHemming <kyhemming@gmail.com>
2024-12-04 11:06:01 +0000

    Day 4: Part 1

---
+++ 4/src/main.rs
@@ -0,0 +1,109 @@
+const INPUT: &str = include_str!(".././input");
+
+// Part1: mul\(\d{1,3},\d{1,3}\)
+// Part2: (mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\))
+
+fn main() {
+    part1();
+}
+
+fn part1() {
+    let rows:Vec<&str> = INPUT.trim().split("\n").collect();
+    let mut word_search:Vec<Vec<&str>> = vec![];
+    for i in 0..rows.len() {
+        word_search.push(Vec::new());
+        let cols:Vec<&str> = rows[i].trim().split("").collect();
+        for j in 0..cols.len() {
+            if (cols[j].len() > 0) {
+                word_search[i].push(cols[j]);
+            }
+        }
+    }
+    let mut hits = 0;
+    for rows in 0..word_search.len() {
+        for cols in 0..word_search[0].len() {
+            if word_search[rows][cols] != "X" {
+                continue;
+            }
+            // // Left to Right
+            if cols <= word_search[0].len()-4 {
+                if (word_search[rows][cols+1] == "M")
+                    & (word_search[rows][cols+2]=="A")
+                    & (word_search[rows][cols+3]=="S") {
+                        println!("{}, {} LR", cols, rows);
+                        hits += 1;
+                    }
+            }
+            // // Right to Left
+            if cols >= 3 {
+                if (word_search[rows][cols-1] == "M")
+                    & (word_search[rows][cols-2] == "A")
+                    & (word_search[rows][cols-3] == "S") {
+                        println!("{}, {} RL", cols, rows);
+                        hits += 1;
+                }
+            }
+            // Vertical
+            // // T to B
+            if rows <= word_search.len()-4 {
+                if (word_search[rows+1][cols] == "M")
+                    & (word_search[rows+2][cols] == "A")
+                    & (word_search[rows+3][cols] == "S") {
+                        println!("{}, {} TB", cols, rows);
+                        hits += 1;
+                    }
+            }
+            // // B to T
+            if rows >= 3 {
+                if (word_search[rows-1][cols] == "M") 
+                    & (word_search[rows-2][cols] == "A")
+                    & (word_search[rows-3][cols] == "S") {
+                        println!("{}, {} BT", cols, rows);
+                        hits += 1;
+                    }
+            }
+
+            // Diagonal
+            // // TR to BL
+            if (cols >= 3) & (rows <= word_search[0].len()-4) {
+                if (word_search[rows+1][cols-1] == "M") 
+                    & (word_search[rows+2][cols-2] == "A")
+                    & (word_search[rows+3][cols-3] == "S") {
+                        println!("{}, {} TRBL", cols, rows);
+                        hits += 1;
+                    }
+            }
+
+            // // TL to BR
+            if (cols <= word_search[0].len()-4) & (rows <= word_search[0].len()-4) {
+                if (word_search[rows+1][cols+1] == "M") 
+                    & (word_search[rows+2][cols+2] == "A")
+                    & (word_search[rows+3][cols+3] == "S") {
+                        println!("{}, {} TLBR", cols, rows);
+                        hits += 1;
+                    }
+            }
+
+            // // BR to TL
+            if (rows >= 3) & (cols >= 3) {
+                if (word_search[rows-1][cols-1] == "M")
+                & (word_search[rows-2][cols-2] == "A")
+                & (word_search[rows-3][cols-3] == "S") {
+                    println!("{}, {} BRTL", cols, rows);
+                    hits += 1;
+                }
+            }
+
+            // // BL to TR
+            if (rows >= 3) & (cols <= word_search[0].len()-4) {
+                if (word_search[rows-1][cols+1] == "M")
+                & (word_search[rows-2][cols+2] == "A")
+                & (word_search[rows-3][cols+3] == "S") {
+                    println!("{}, {} BLTR", cols, rows);
+                    hits += 1;
+                }
+            }
+        }
+    }
+    println!("{} hits", hits);
+}
