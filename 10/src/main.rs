8183e49da3abcdf69e9405e931b56471b93c2869
KaiHemming <kyhemming@gmail.com>
2024-12-10 17:20:31 +0000

    Day 10

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -11,9 +11,8 @@ fn part1() {
             x.to_digit(10).unwrap()
         }).collect::<Vec<u32>>()
     }).collect::<Vec<Vec<u32>>>();
-    // println!("{:?}", map);
-    
-    let mut trailheads:HashSet::<(usize,usize)> = HashSet::<(usize,usize)>::new();
+    let mut part_1_total:u32 = 0;
+    let mut part_2_total:u32 = 0;
     map.clone().into_iter()
         .enumerate()
         .for_each(|(row,r)| {
@@ -22,62 +21,67 @@ fn part1() {
             .enumerate()
             .for_each(|(col, x)| {
                 if x == 0 {
-                    // println!("NEW {}: {}, {}", x, row, col);
-                    if traverse((row,col), &map) {
-                        trailheads.insert((row,col));
-                    }
+                    let results = traverse((row,col), &map);
+                    part_1_total += results.0 as u32;
+                    part_2_total += results.1 as u32;
                 }
             })
         });
-    
-    println!("{}", trailheads.len());
+    println!("{}", part_1_total);
+    println!("{}", part_2_total);
 }
 
-fn traverse(pos: (usize,usize), map: &Vec<Vec<u32>>) -> bool {
-    let cur_num = map[pos.0][pos.1];
-    // println!("{}: {:?}", cur_num, pos);
-    // check reached end of trail
-    if cur_num == 9 {
-        // println!("Found end");
-        return true;
-    }
-    // check up
-    if pos.0 > 0 {
-        if map[pos.0-1][pos.1] == cur_num+1 {
-            // println!("Up");
-            if traverse((pos.0-1, pos.1), map) {
-                return true;
+fn traverse(pos: (usize,usize), map: &Vec<Vec<u32>>) -> (usize, usize) {
+    let mut incomplete_journeys: Vec<Vec<(usize,usize)>> = vec![vec![pos]];
+    let mut complete_journeys: Vec<Vec<(usize,usize)>> = vec![];
+    let mut unique_destinations: HashSet<(usize,usize)> = HashSet::<(usize,usize)>::new();
+    while incomplete_journeys.len() > 0 {
+        let first_journey: Vec<(usize,usize)> = incomplete_journeys[0].clone();
+        let cur_pos = first_journey[first_journey.len()-1];
+        let cur_num = map[cur_pos.0][cur_pos.1];
+
+        // check reached end of trail
+        if cur_num == 9 {
+            complete_journeys.push(first_journey.clone());
+            incomplete_journeys.remove(0 as usize);
+            unique_destinations.insert(cur_pos);
+            continue;
+        }
+        // check up
+        if cur_pos.0 > 0 {
+            if map[cur_pos.0-1][cur_pos.1] == cur_num+1 {
+                let mut new_journey = first_journey.clone();
+                new_journey.push((cur_pos.0-1, cur_pos.1));
+                incomplete_journeys.push(new_journey);
             }
         }
-    }
-    // check down
-    if pos.0 < map[0].len()-1 {
-        if map[pos.0+1][pos.1] == cur_num+1 {
-            // println!("Down");
-            if traverse((pos.0+1, pos.1), map) {
-                return true;
+        // check down
+        if cur_pos.0 < map[0].len()-1 {
+            if map[cur_pos.0+1][cur_pos.1] == cur_num+1 {
+                let mut new_journey = first_journey.clone();
+                new_journey.push((cur_pos.0+1, cur_pos.1));
+                incomplete_journeys.push(new_journey);
             }
         }
-    }
-    // check left
-    if pos.1 > 0 {
-        if map[pos.0][pos.1-1] == cur_num+1 {
-            // println!("Left");
-            if traverse((pos.0, pos.1-1), map) {
-                return true;
+        // check left
+        if cur_pos.1 > 0 {
+            if map[cur_pos.0][cur_pos.1-1] == cur_num+1 {
+                let mut new_journey = first_journey.clone();
+                new_journey.push((cur_pos.0, cur_pos.1-1));
+                incomplete_journeys.push(new_journey);
             }
         }
-    }
-    // check right
-    if pos.1 < map.len()-1 {
-        if map[pos.0][pos.1+1] == cur_num+1 {
-            // println!("Right");
-            if traverse((pos.0, pos.1+1), map) {
-                return true;
+        // check right
+        if cur_pos.1 < map.len()-1 {
+            if map[cur_pos.0][cur_pos.1+1] == cur_num+1 {
+                let mut new_journey = first_journey.clone();
+                new_journey.push((cur_pos.0, cur_pos.1+1));
+                incomplete_journeys.push(new_journey);
             }
         }
+        incomplete_journeys.remove(0 as usize);
     }
-    return false;
+    return (unique_destinations.len(),complete_journeys.len());
 }
 
 fn part2() {
