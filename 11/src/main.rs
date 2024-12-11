7d662c36543843de2e17cfdbf3ad8a52042e21d3
KaiHemming <kyhemming@gmail.com>
2024-12-11 18:46:29 +0000

    Day 11: Part 2

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -1,7 +1,8 @@
 const INPUT: &str = include_str!(".././input");
+use std::collections::HashMap;
 
 fn main() {
-    part1();
+    part2();
 }
 
 fn part1() {
@@ -9,8 +10,7 @@ fn part1() {
         .split(" ")
         .map(|x| x.parse::<u64>().unwrap())
         .collect();
-    for i in 0..75 {
-        println!("{}", i);
+    for _i in 0..25 {
         let mut j = 0;
         while j < stones.len() {
             if stones[j] == 0 {
@@ -47,5 +47,45 @@ fn split_digit(x:&u64) -> (u64,u64) {
 }
 
 fn part2() {
+    let mut stones:HashMap<u64,u64> = HashMap::<u64,u64>::new();
+    INPUT.trim()
+        .split(" ")
+        .for_each(|x| {
+            let num = x.parse::<u64>().unwrap();
+            stones.entry(num).or_default();
+            let cur_count:u64 = *stones.get(&num).unwrap();
+            stones.insert(num, cur_count+1);
+        });
+    for _i in 0..75 {
+        let mut new_stones:HashMap<u64,u64> = HashMap::<u64,u64>::new();
+        for (key, value) in &stones {
+            if *key == 0 {
+                new_stones.entry(1).or_default();
+                let cur_count:u64 = *new_stones.get(&1).unwrap();
+                new_stones.insert(1, cur_count+value);
+            }
+            else if get_num_digits(key)%2 == 0 {
+                let new_digits = split_digit(key);
+                
+                new_stones.entry(new_digits.0).or_default();
+                let cur_count:u64 = *new_stones.get(&new_digits.0).unwrap();
+                new_stones.insert(new_digits.0, cur_count+value);
 
+                new_stones.entry(new_digits.1).or_default();
+                let cur_count:u64 = *new_stones.get(&new_digits.1).unwrap();
+                new_stones.insert(new_digits.1, cur_count+value);
+            }
+            else {
+                new_stones.entry(*key*2024).or_default();
+                let cur_count:u64 = *new_stones.get(&(key*2024)).unwrap();
+                new_stones.insert(*key*2024, cur_count+value);
+            }
+        }
+        stones = new_stones;
+    }
+    let mut total = 0;
+    stones.into_iter().for_each(|(_key,value)| {
+        total += value;
+    });
+    println!("{}", total);
 }
